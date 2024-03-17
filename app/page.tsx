701b1bc8b957a371f3fe828d897fa0d7e92820a7
Victor Lobato <pvlobato@gmail.com>
2024-03-17 12:17:56 +0100

    Add signout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Button, withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import TodoList from "@/components/TodoList";
+import { signOut } from "aws-amplify/auth";
 
 function App() {
+  async function handleSignOut() {
+    try {
+      await signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error)
+    }
+  }
   return (
     <>
       <h1>Hello, Amplify!</h1>
       <TodoList />
+      <Button
+        onClick={handleSignOut}
+      >
+        Sign Out
+      </Button>
     </>
   )
 }
